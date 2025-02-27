0c94901f49fcf1e1aa374d359ed3e263018b68f2
Cauã Paduani <cauapaduani@gmail.com>
2025-02-27 03:13:56 -0300

    the third party contract implemented

---
+++ Tokentonftmint.sol
@@ -0,0 +1,49 @@
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC20/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC721Receiver.sol";
+import "./ERC721mintable.sol";
+
+contract NFTMinterAndStaker {
+
+    
+
+    IERC20 public paymentoken;
+    AnNft public nftcontract;
+    uint public mintprice = 10 *(10 ** 18);
+    uint public rewardRate = 10 *(10 ** 18);
+    mapping(uint256 => address) public stakedby;
+    mapping(uint256 => address) public stakingStart;
+
+    constructor(address _paymentToken, address _nftcontract){
+        _paymentToken = IERC20(_paymentToken);
+        nftcontract = AnNft(_nftcontract);
+
+    }
+    function mintNFT(uint256 tokenId,string memory metadataURI) external {
+        require(paymentToken.transferFrom(msg.sender, address(this), mintprice,"payment fayled"));
+        nftcontract.mint(msg.sender, tokenId, metadataURI);
+    }
+
+    function stakeNFT(uint256 tokenId) external {
+        require(nftcontract.ownerOf(tokenId) == msg.sender,"not the owner");
+         nftContract.transferFrom(msg.sender, address(this), tokenId);
+         stakedby[tokenId] = msg.sender;
+         stakingStart[tokenId] = block.timestamp;
+    }
+
+    function withdrawNFT(uint256 tokenId) external {
+        require(stakedby[tokenId] == msg.sender,"not the owner");
+        nftcontract.transferFrom(address(this), msg.sender, tokenId);
+        stakedby[tokenId] = address(0);
+        stakingStart[tokenId] = 0;
+    }
+    function claimRewards(uint256 tokenId) external {
+        require(stakedby[tokenId] == msg.sender,"not the owner");
+        uint256 stakedTime = block.timestamp - stakingStart[tokenId];
+        uint reward = (stakedTime / 1 days) *rewardRate;
+        stakingStart[tokenId] = block.timestamp;
+        paymentoken.transfer(msg.sender, reward);
+    }
+
+}
