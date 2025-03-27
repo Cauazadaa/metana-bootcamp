b471b45adcc64f0a6973aa38f304be16ee6c26cb
Cauã Paduani <cauapaduani@gmail.com>
2025-03-26 21:09:53 -0300

    adding guess the number test

---
+++ module-6/CaptureTheEtherHardhat/test/04-GuessTheNumber.test.ts
@@ -0,0 +1,31 @@
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import { expect } from 'chai';
+import { Contract } from 'ethers';
+import { ethers } from 'hardhat';
+import { parseEther } from 'viem';
+const { utils, provider } = ethers;
+
+describe('GuessTheNumberChallenge', () => {
+  let target: Contract;
+  let deployer: SignerWithAddress;
+  let attacker: SignerWithAddress;
+
+  before(async () => {
+    [attacker, deployer] = await ethers.getSigners();
+
+    target = await (
+      await ethers.getContractFactory('GuessTheNumberChallenge', deployer)
+    ).deploy({
+      value: utils.parseEther('1'),
+    });
+
+    await target.deployed();
+
+    target = target.connect(attacker);
+  });
+
+  it('exploit', async () => {
+    const tx = await target.connect(attacker).guess(42 , {value : utils.parseEther('1')});
+    expect(await provider.getBalance(target.address)).to.equal(0);
+  });
+});
