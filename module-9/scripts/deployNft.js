4c7745fdcfb28525dfd6fc47858d58b425a9ad36
Cauã Paduani <cauapaduani@gmail.com>
2025-04-24 14:13:51 -0300

    fixing mistakes

--- module-9/scripts/deployNft.js
+++ module-9/scripts/deployNft.js
@@ -1,11 +1,11 @@
 const {ethers, upgrades} = require("hardhat");
 
 async function  main() {
-    const nft = await ethers.getContractFactory("erc721");
-    const NFT_deploy = await upgrades.deployProxy(nft, [nftoken],[NTF], {
-        initializer : "initializer"
+    const nft = await ethers.getContractFactory("AnNft");
+    const NFT_deploy = await upgrades.deployProxy(nft, ["nftoken","NTF"], {
+        initializer : "initialize"
     });
-    await NFT_deploy.deployed();
+    await NFT_deploy.waitForDeployment();
     console.log("nft deployed at : ",nft.address);
 
 }
--- module-9/scripts/deployerc20.js
+++ module-9/scripts/deployerc20.js
@@ -5,7 +5,7 @@ async function main() {
     const erc20 = await upgrades.deployProxy(erc,["token"],["TKN"],{
         initializer : "initialize"
     });
-    await erc20.deployed();
+    await erc20.waitForDeployment();
     console.log("ERC20 deployed to: ", erc20.address);
 }
 main().catch((error)=> {
